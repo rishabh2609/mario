c1934b52767322f3f553aecae362161141207f98
rishabh2609 <rishabhchanana97@gmail.com>
2018-01-05 15:51:12 +0530

    portrait mode on phone fixed

--- scripts/game.js
+++ scripts/game.js
@@ -15,12 +15,7 @@ window.addEventListener("load", function() {
 	.setup({
 		scaleToFit: true,
 		width: w,
-		height: h,
-		upsampleWidth:  420,  // Double the pixel density of the 
-		upsampleHeight: 320,  // game if the w or h is 420x320
-		                    // or smaller (useful for retina phones)
-		downsampleWidth: 1024, // Halve the pixel density if resolution
-		downsampleHeight: 768 
+		height: h
 	}).controls(true).touch();
 	
 	Q.setImageSmoothing(false);
